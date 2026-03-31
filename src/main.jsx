99725706089d8c6c7b58c3cdf29b8c02cc78a2b2
Nishit <nishit@example.com>
2026-03-31 22:32:51 +0530

    STABILITY_V5: Absolute Minimum Build - Removed Postprocessing and StrictMode to fix Blue Screen

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Removing StrictMode to prevent double-mounting which can destabilize Three.js contexts
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
